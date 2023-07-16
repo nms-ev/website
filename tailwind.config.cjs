d904299c5af9c6470c04dd89bbe72d2b16db3327
Niccolo Borgioli <hi@nicco.io>
2023-07-16 23:09:18 +0200

    move components to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,5 +16,34 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        '.copy-sm': {
+          [`@apply font-copy font-light text-sm`]: {},
+        },
+        '.copy-md': {
+          [`@apply font-copy font-light text-base`]: {},
+        },
+        '.copy-md-strong': {
+          [`@apply font-copy font-medium text-base`]: {},
+        },
+        '.copy-lg': {
+          [`@apply font-copy font-light text-2xl`]: {},
+        },
+        '.copy-lg-strong': {
+          [`@apply font-copy font-medium text-2xl`]: {},
+        },
+        '.copy-xl': {
+          [`@apply font-copy font-light text-[40px]`]: {},
+        },
+        '.copy-xxl': {
+          [`@apply font-copy font-light text-[96px]`]: {},
+        },
+        '.mono-md': {
+          [`@apply font-mono font-normal text-base`]: {},
+        },
+      })
+    },
+  ],
 }
